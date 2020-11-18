2dda503baa23e1007f089ab8d3af35c978f75957
Simon Lopez <simon16143@gmail.com>
2020-11-17 22:36:19 -0500

    actualizando

--- sqlite3.py
+++ sqlite3.py
@@ -1,9 +1,9 @@
 import sqlite3
+#El objeto de conexion se crea en la vairable conn
+conn = sqlite3.connect('Second')
 
-conn = sqlite3.connect('First')
 c = conn.cursor()
-c.execute('''CREATE TABLE tasks(id INTEGER PRIMARY KEY,name TEXT NOT NULL,priority INTEGER NOT NULL
-);''')
+c.execute('''CREATE TABLE task(id INTEGER PRIMARY KEY,name TEXT NOT NULL,priority INTEGER NOT NULL);''')
 
 
-c.execute('INSERT INTO tasks(id,name,priority) VALUES(?,?,?)',(0,'My first Task',1))
+c.execute('INSERT INTO task(name,priority) VALUES (?,?)',('PRUEBAx',223))
